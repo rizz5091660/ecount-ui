69ff99a41b1d71bc013e370bbcc1b26a0e033976
Rizky <rizky.akbar.sani@gmail.com>
2018-11-10 18:47:22 +0800

    Switch to smart table

--- src/app/pages/accounts/sales/sales-form/sales-form.component.ts
+++ src/app/pages/accounts/sales/sales-form/sales-form.component.ts
@@ -12,49 +12,58 @@ export class SalesFormComponent implements OnInit {
   model:SalesOrder;
   trxnDtNgb: NgbDateStruct;
   dueDtNgb: NgbDateStruct;
-  soDetails:SalesOrderDetail[]=[
-{
-  id:null,
-  name:null,
-  description:null,
-  quantity:null,
-  unitPrice:null,
-  taxAmount:null,
-  trxnAmount:null,
-  discount:null,
-  inventoryId:null
-},
-{
-  id:null,
-  name:null,
-  description:null,
-  quantity:null,
-  unitPrice:null,
-  taxAmount:null,
-  trxnAmount:null,
-  discount:null,
-  inventoryId:null
-},
-{
-  id:null,
-  name:null,
-  description:null,
-  quantity:null,
-  unitPrice:null,
-  taxAmount:null,
-  trxnAmount:null,
-  discount:null,
-  inventoryId:null
-}];
+  
   constructor() { }
 
   ngOnInit() {
     this.model = new SalesOrder();
+    this.model.salesOrderDetails=[
+      {
+        id:1,
+        name:null,
+        description:null,
+        quantity:null,
+        unitPrice:null,
+        taxAmount:null,
+        trxnAmount:null,
+        discount:null,
+        inventoryId:null
+      },
+      {
+        id:2,
+        name:null,
+        description:null,
+        quantity:null,
+        unitPrice:null,
+        taxAmount:null,
+        trxnAmount:null,
+        discount:null,
+        inventoryId:null
+      },
+      {
+        id:3,
+        name:null,
+        description:null,
+        quantity:null,
+        unitPrice:null,
+        taxAmount:null,
+        trxnAmount:null,
+        discount:null,
+        inventoryId:null
+      }];
   }
+
 
   onAddSalesDetail(){
     let sod = new SalesOrderDetail();
-    this.soDetails.push(sod);
+    this.model.salesOrderDetails.push(sod);
+  }
+
+  onCalculateTotalTrxn(){
+    this.model.totalAmount=0;
+    this.model.totalAmount= this.model.salesOrderDetails.filter((sod) =>sod.trxnAmount)
+    .map((sod) => +sod.trxnAmount)
+    .reduce((sum, current) => sum + current);
   }
 
 }
--- src/app/pages/maintenance/coa/coa.component.ts
+++ src/app/pages/maintenance/coa/coa.component.ts
@@ -5,6 +5,8 @@ import { Observable } from 'rxjs';
 import { Coa } from '../../../class/coa';
 import { CoaService } from '../../../service/coa.service';
 import { HttpResponseWS } from '../../../class/htt_response_ws';
+import { DropDownModel } from '../../../class/drop_down';
+import { LocalDataSource } from 'ng2-smart-table';
 
 @Component({
   selector: 'coa',
@@ -15,15 +17,55 @@ export class CoaComponent implements OnInit {
   radioModel  = 'all';
   observables : Observable<Coa[]>;
   observable : Observable<Coa>;
-  coas : Coa[];
+  coas : Coa[]
   accountType:number;
-  model:Coa;
-  modelDD:Coa;
+  model:Coa = new Coa();
+  modelDD:Coa = new Coa();
   closeResult:string;
   httpRespObservable:Observable<HttpResponseWS>;
   coaCode:string;
+  source:LocalDataSource = new LocalDataSource();
 
   constructor(private modalService : NgbModal, private service : CoaService ) { }
+
+  settings = {
+    add: {
+      addButtonContent: '<i class="nb-plus"></i>',
+      createButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    edit: {
+      editButtonContent: '<i class="nb-edit"></i>',
+      saveButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    delete: {
+      deleteButtonContent: '<i class="nb-trash"></i>',
+      confirmDelete: true,
+    },    
+    columns: {
+      l1AccountTypName: {
+        title: 'Account Type',
+        type: 'string',
+      },
+      coaCd: {
+        title: 'Code',
+        type: 'string',
+      },
+      name: {
+        title: 'Name',
+        type: 'string',
+      },
+      description: {
+        title: 'Description',
+        type: 'string',
+      },
+      taxName: {
+        title: 'Tax Type',
+        type: 'string',
+      }
+    },
+  };
 
   ngOnInit() {
     this.loadListCoa('all','');  
@@ -34,6 +76,7 @@ export class CoaComponent implements OnInit {
     this.observables = this.service.getCoaAll(accType,coaCode); 
     this.observables.subscribe((listObservable) => {
       this.coas = listObservable;
+      this.source.load(this.coas);
     })
   }
 
@@ -45,44 +88,61 @@ export class CoaComponent implements OnInit {
   }
 
   onSubmit(){
-    if(this.model.id==null){
-      this.onCreate();
-    }else{
-      this.onUpdate();
-    }
+     if(this.model.id==null){
+       this.onCreate();
+     }else{
+       this.onUpdate();
+     }
 
   }
 
   onCreate(){
-    console.log(this.model);
+    let typeDD:DropDownModel=this.modelDD.accountTypes.find(i=>i.id ==String(this.model.l1AccountType));
+    let taxDD:DropDownModel =this.modelDD.taxes.find(i=>i.id==String(this.model.tax));
+    this.model.l1AccountTypName = typeDD.name;
+    this.model.taxName = taxDD.name;
     this.httpRespObservable = this.service.create(this.model);
     this.httpRespObservable.subscribe((httpRespObservable)=>{
-      console.log(httpRespObservable.status);
+      //console.log(httpRespObservable.status);
+      this.coas.push(this.model);
+      this.source.load(this.coas);
     })
   }
 
   onUpdate(){
     this.httpRespObservable = this.service.update(this.model);
     this.httpRespObservable.subscribe((httpRespObservable)=>{
-      console.log(httpRespObservable.status);
+     // console.log(httpRespObservable.status);
     })
   }
 
-  onEdit(modal,coa:Coa){
-    this.openModal(modal);
-    this.model = coa;
-  }
-     
-  onDelete(id:number){
-    this.httpRespObservable = this.service.delete(id);
-    this.httpRespObservable.subscribe((httpRespObservable)=>{
-      console.log(httpRespObservable.status);
-    });
+  onEdit(event): void{
+    // this.openModal(modal);
+    // this.model = coa;
+    console.log("Supp");
   }
 
-  onChange(value) {
-    console.log(value);
-   }
+  onEdit2(event): void{
+    console.log(event);
+  }
+
+  onDeleteConfirm(event): void {
+    if (window.confirm('Are you sure you want to delete?')) {
+      this.onDelete(event.data);
+      event.confirm.resolve();
+    } else {
+      event.confirm.reject();
+    }
+  }
+     
+  onDelete(coa:Coa){
+    this.httpRespObservable = this.service.delete(coa.id);
+    this.httpRespObservable.subscribe((httpRespObservable)=>{
+     // console.log(httpRespObservable.status);
+      this.coas.splice(this.coas.indexOf(coa),1);
+      this.source.load(this.coas);
+    });
+  }
 
   onSelectTab(val){
     this.loadListCoa(val,'');
@@ -107,7 +167,5 @@ export class CoaComponent implements OnInit {
       return `with: ${reason}`;
     }
   }
-
- 
 
 }
