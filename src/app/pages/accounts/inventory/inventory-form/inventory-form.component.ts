fd9a2e9274fae82613edacc60e7f53d4a078d3ba
Rizky <rizky.akbar.sani@gmail.com>
2018-12-24 06:46:31 +0800

    Inventory

--- src/app/pages/accounts/inventory/inventory-form/inventory-form.component.ts
+++ src/app/pages/accounts/inventory/inventory-form/inventory-form.component.ts
@@ -5,6 +5,7 @@ import { Coa } from '../../../../class/coa';
 import { Tax } from '../../../../class/tax';
 import { InventoryService } from '../../../../service/inventory.service';
 import { Observable } from 'rxjs';
+import { InventoryBalance } from '../../../../class/inventory_balance';
 
 @Component({
   selector: 'inventory-form',
@@ -14,6 +15,7 @@ import { Observable } from 'rxjs';
 export class InventoryFormComponent implements OnInit {
   msgs:Messages[]=[];
   model:Inventory;
+  types:any[];
   modelObs:Observable<Inventory>;
   constructor(private inventoryService:InventoryService) { }
 
@@ -21,6 +23,7 @@ export class InventoryFormComponent implements OnInit {
     this.model = new Inventory();
     
     this.modelObs=this.inventoryService.init();
+    this.model.invBalance= new InventoryBalance();
     this.model.coaSales = new Coa();
     this.model.taxSales = new Tax();
     this.model.coaPurchase = new Coa();
@@ -31,7 +34,15 @@ export class InventoryFormComponent implements OnInit {
      }
     );
 
+    this.types = [
+      {label: 'Inventory', value: 'I', icon: 'fa fa-fw fa-cc-paypal'},
+      {label: 'Non Inventory', value: 'NI', icon: 'fa fa-fw fa-cc-visa'},
+      {label: 'Services', value: 'S', icon: 'fa fa-fw fa-cc-mastercard'}
+  ];
 
   }
 
+  onChangeOption(event:any){
+    console.log("------> "+event.value);
+  }
 }
--- src/app/pages/accounts/inventory/inventory-search/inventory-search.component.ts
+++ src/app/pages/accounts/inventory/inventory-search/inventory-search.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { Message } from '../../../../components/common/message';
 import { InventoryService } from '../../../../service/inventory.service';
 import { Inventory } from '../../../../class/inventory';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
+import { InventoryBalance } from '../../../../class/inventory_balance';
+import { Coa } from '../../../../class/coa';
+import { Tax } from '../../../../class/tax';
+import { HttpResponseWS } from '../../../../class/http_response_ws';
+import { SelectItem } from '../../../../components/common/selectitem';
+import { ConfirmationService, Message } from '../../../../components/common/api';
+
 
 @Component({
   selector: 'inventory-search',
@@ -15,37 +21,127 @@ export class InventorySearchComponent implements OnInit {
   cols: any[];
   model:Inventory;
   inventories:Inventory[];
-  modelObs:Observable<Inventory[]>;
+  display:boolean;
+  types:any[];
+  httpResponseWS:HttpResponseWS;
+  coas:SelectItem[];
+  taxes:SelectItem[];
 
-  constructor(private inventoryService:InventoryService, private router:Router) { }
+  constructor(private inventoryService:InventoryService,private confirmationService: ConfirmationService, private router:Router) { }
 
   ngOnInit() {
-    this.model = new Inventory();
     this.inventories = [];
     this.cols = [
+      { field: 'code', header: 'Code', type: 'txt' },
       { field: 'name', header: 'Name', type: 'txt' },
       { field: 'invBalance.qty', header: 'Quantity', type: 'txt' },
       { field: 'invBalance.balance', header: 'Balance', type: 'txt' },      
       { field: 'unitPricePurchase', header: 'Price Purchase', type: 'txt' },
       { field: 'unitPriceSales', header: 'Price Sales', type: 'txt' },
     ];
-    this.init();
+    this.types = [
+      {label: 'Inventory', value: 'I', icon: 'fa fa-fw fa-cc-paypal'},
+      {label: 'Non Inventory', value: 'NI', icon: 'fa fa-fw fa-cc-visa'},
+      {label: 'Services', value: 'S', icon: 'fa fa-fw fa-cc-mastercard'}
+  ];
+    this.initObject();
+    this.initData();
   }
 
-  init(){
-    this.modelObs=this.inventoryService.getAll();
-    this.modelObs.subscribe((obs)=>{
+  initObject(){
+    this.model = new Inventory();
+    this.model.invBalance= new InventoryBalance();
+    this.model.coaInv = new Coa();
+    this.model.coaSales = new Coa();
+    this.model.taxSales = new Tax();
+    this.model.coaPurchase = new Coa();
+    this.model.taxPurchase = new Tax();   
+  }
+
+  initData(){
+    let modelObsList:Observable<Inventory[]>=this.inventoryService.getAll();
+    modelObsList.subscribe((obs)=>{
       this.inventories=obs;
+     }
+    );
+
+    let modelObs:Observable<Inventory>=this.inventoryService.init();
+     modelObs.subscribe((obs)=>{
+      this.coas = obs.coas;
+      this.taxes = obs.taxes;
      }
     );
   }
 
   onRowSelect(event){
-    this.router.navigate(["/pages/accounts/inventory/inventory-form"],{ queryParams: { type: null, stage: null } });
+    let invTemp:Inventory= new Inventory();
+    this.inventories.filter(function(inv){
+      if(inv.id==event.data.id)
+          invTemp = inv;
+      }
+    );
+   this.model= invTemp;
+   this.display=true;
   }
 
   onAdd(){
-    this.router.navigate(["/pages/accounts/inventory/inventory-form"],{ queryParams: { type: null, stage: null } });
+  this.initObject();  
+  this.display=true;
   }
 
+  onSave(){
+    if(this.model.id>0){
+      this.onUpdate();
+    }else{
+      this.onCreate();
+    }
+  }
+
+  onCreate(){
+    let httpWSObs:Observable<HttpResponseWS>=this.inventoryService.create(this.model);
+    this.onProcessResponse(httpWSObs,"Create");
+  }
+
+  onUpdate(){
+    let httpWSObs:Observable<HttpResponseWS>=this.inventoryService.update(this.model);
+    this.onProcessResponse(httpWSObs,"Update");
+
+  }
+
+  onDelete(){
+    let httpWSObs:Observable<HttpResponseWS>=this.inventoryService.delete(this.model.id);
+    this.onProcessResponse(httpWSObs,"Delete");
+  }
+
+  onDeleteConfirm() {
+    this.display = false;
+    this.confirmationService.confirm({ 
+      message: 'Are you sure that you want to proceed?',
+      header: 'Confirmation',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.onDelete();
+      },
+      reject: () => {
+        // this.msgs = [{ severity: 'error', summary: 'Rejected', detail: 'You have rejected' }];
+      }
+    });
+  }
+
+  onProcessResponse(httpWSObs:Observable<HttpResponseWS>,type:string){
+    httpWSObs.subscribe((httpWSObs)=>
+    {
+      this.httpResponseWS = httpWSObs;
+      if(type=="Create"){
+        this.model.id=this.httpResponseWS.value;
+      }
+      this.ngOnInit();
+      this.msgs = [{ severity: 'success', summary: 'Confirmed', detail: 'Successfully ' + type }];
+    },
+    error => {
+      this.msgs = [{ severity: 'error', summary: 'Confirmed', detail: 'System Error' }];
+    }
+  );
+  this.display=false;
+  }
 }
